304a94891ea4d0a9c2984e52201aace6a276fc80
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-07 15:22:02 +0300

    final commit

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50, true);
+        System.out.println("initial page count = "+printer.getPagesPrinted());
+
+        //using formatters in the text so that the text comes out readable and understand able
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current job Pages: %d, Printer Total: %d %n", pagesPrinted, printer.getPagesPrinted());
+
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current job Pages: %d, Printer Total: %d %n", pagesPrinted, printer.getPagesPrinted());
+    }
+
+
+}
