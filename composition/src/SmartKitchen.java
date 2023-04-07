4ea09511acd7bb0ce3663b4832cff77ff80aa37a
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-07 14:03:30 +0300

    first commit

---
+++ composition/src/SmartKitchen.java
@@ -0,0 +1,23 @@
+public class SmartKitchen {
+    private CoffeeMaker brewMaster;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+}
