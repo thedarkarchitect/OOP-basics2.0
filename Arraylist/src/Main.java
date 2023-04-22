91441cb6e0aee82932392ea64ae51d8aaa570c83
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-22 17:01:36 +0300

    first commit

---
+++ Arraylist/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Available actions: ");
+        System.out.println("0 - to shutdown");
+        System.out.println("1 - to add items(s) to list (comma delimited list)");
+        System.out.println("2 - to remove any items (comma delimited list)");
+
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter a number for which action you want to do: ");
+
+        
+    }
+}
