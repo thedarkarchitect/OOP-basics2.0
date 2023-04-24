0469cb0406b3f65db90277d54c3c432ba6fee41f
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-25 00:34:14 +0300

    fourth commit

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -2,8 +2,7 @@ import java.util.LinkedList;
 
 public class Main {
     record Place (String town, int distance ){
-        //make the record display a certain way
-
+        //make the record display a certain way.
         @Override
         public String toString() {
             return String.format("%s (%d)", town, distance);
@@ -13,27 +12,37 @@ public class Main {
     public static void main(String[] args) {
         LinkedList<Place> locations = new LinkedList<>();
         Place adelaide = new Place("Adelaide", 1374);
-        addPlace(locations, adelaide);
+        addPlace(locations, new Place("adelaide", 1374));
+        addPlace(locations, new Place("Brisbane", 917));
+        addPlace(locations, new Place("Perth", 3923));
+        addPlace(locations, new Place("Alice Springs", 2771));
+        addPlace(locations, new Place("Darwin", 3972));
+        addPlace(locations, new Place("Melbourne", 877));
         System.out.println(locations);
-
-//        location.add(new PLace("Adelaide", 1374));
-//        location.add(new PLace("Alice Springs", 2771));
-//        location.add(new PLace("Brisbane", 917));
-//        location.add(new PLace("Darwin", 3972));
-//        location.add(new PLace("Melbourne", 877));
-//        location.add(new PLace("Perth", 3923));
-////        program(location);
-//        Iterator<PLace> show = location.iterator();
-//
-//        System.out.println(show.next());
-//        System.out.println(show.pre);
 
     }
 
     private static void addPlace(LinkedList<Place> list, Place place){
-        if(list.contains(place)){
-            System.out.println("Found duplicate: "+ place);
+
+        if(list.contains(place)){//this checks if there is already a similar record
+            System.out.println("Found duplicate: "+ place);//show the item
             return;
+        }
+
+        for(Place p : list){//for the record in the list
+            if(p.town().equalsIgnoreCase(place.town())){//checks if the record p is equal to the a record with a similar name ignore the cases
+                System.out.println("Found duplicate: "+place);//show the record
+                return;
+            }
+        }
+
+        int matchedIndex = 0;
+        for(var listPlace: list){
+            if(place.distance() < listPlace.distance()){
+                list.add(matchedIndex, place);
+                return;
+            }
+            matchedIndex++;
         }
         list.add(place);
     }
