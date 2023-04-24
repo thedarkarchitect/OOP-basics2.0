d5c4bb1c504f96ef2052d2bd0869b76dea52e0a0
thedarkarchitect <romanreynolds240@gmail.com>
2023-04-24 23:46:29 +0300

    First commit

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Main {
+    record PLace (String town, int distance ){}
+
+    public static void main(String[] args) {
+        LinkedList<PLace> location = new LinkedList<>();
+//        location.add(new PLace("Adelaide", 1374));
+//        location.add(new PLace("Alice Springs", 2771));
+//        location.add(new PLace("Brisbane", 917));
+//        location.add(new PLace("Darwin", 3972));
+//        location.add(new PLace("Melbourne", 877));
+//        location.add(new PLace("Perth", 3923));
+////        program(location);
+//        Iterator<PLace> show = location.iterator();
+//
+//        System.out.println(show.next());
+//        System.out.println(show.pre);
+
+    }
+
+    private static void addPlace(LinkedList<PLace> list, Place place){
+        
+    }
+
+
+}
